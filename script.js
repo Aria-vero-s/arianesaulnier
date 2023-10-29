1fbbae1612147a693e3e61eaa0f1132d64326bcf
aria-vero-s <99185542+Aria-vero-s@users.noreply.github.com>
2023-10-29 18:57:30 +0000

    Email configuration and style adjustments

--- script.js
+++ script.js
@@ -52,3 +52,48 @@ document.addEventListener('DOMContentLoaded', function () {
     emailIcon.addEventListener('click', toggleEmail);
   });
   
+
+// Email configuration
+
+document.addEventListener('DOMContentLoaded', function() {
+  emailjs.init("BRInTh0xHPku6Lr0u");
+
+  console.log("DOMContentLoaded event fired");
+  const contactForm = document.getElementById("contact-form");
+  const successMessage = document.getElementById("success-message");
+  const errorMessage = document.getElementById("error-message");
+
+  // Add a submit event listener to the form
+  contactForm.addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+      // Get form data
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    console.log("Form submitted");
+
+    // Use the emailjs.sendForm method to send the form
+    emailjs
+      .sendForm("service_djf8d02", "template_lv5j9eq", contactForm)
+      .then(
+        function(response) {
+          console.log("Email sent successfully", response);
+          successMessage.style.display = "block";
+        },
+        function(error) {
+          console.log("Email sending failed", error);
+          console.error(error);
+
+          // Additional debugging information:
+          if (error.response) {
+            console.log("Response data:", error.response.data);
+            console.log("HTTP status:", error.response.status);
+            console.log("Headers:", error.response.headers);
+          }
+          errorMessage.style.display = "block";
+        }
+      );
+  });
+});
