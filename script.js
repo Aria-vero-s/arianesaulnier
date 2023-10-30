8b03c4aedbfcff93f3faf654feb43b2293385db2
aria-vero-s <99185542+Aria-vero-s@users.noreply.github.com>
2023-10-30 18:29:58 +0000

    made navbar responsive

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-function navBar() {
-    var x = document.getElementById("myNavbar");
-    if (x.className === "navbar") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar";
-    }
-}
+document.addEventListener('DOMContentLoaded', function () {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  menuToggle.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+  });  
+});
+
 
 document.addEventListener('DOMContentLoaded', function () {
     // Function to toggle the visibility of a specific case study section
